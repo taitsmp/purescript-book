b628d27d7015485bca8e117233763da96cc19871
Tait Larson <telarson@gmail.com>
2014-11-10 13:16:03 -0500

    messing with ADTs, etc.

--- chapter6/src/C6ex.purs
+++ chapter6/src/C6ex.purs
@@ -2,24 +2,27 @@ module C6ex where
 
 import Data.Array
 
-newtype Complex = Complex 
+-- newtype constructors must take exactly one argument.  This actually works. 
+newtype Complex = Complex
    { real :: Number
    , imaginary :: Number 
    }
 
+-- experimenting with records and types synonyms
+type Cplx = { real :: Number, imaginary :: Number }
 
 instance complexShow :: Show Complex where
   show (Complex c) = "r =  " ++ (show c.real) ++ "i = " ++ (show c.imaginary)
 --  show (Complex { real = r, imaginary = c }) = "r =  " ++ (show c) ++ "i = " ++ (show c)
-
 
 data NonEmpty a = NonEmpty a [a]
 
 instance neSG :: Semigroup (NonEmpty a) where
   (<>) (NonEmpty a as) (NonEmpty b bs) = NonEmpty a ([b] <> as <> bs)
 
--- WTF. why doesn't this work? 
 instance complexSemiGroup :: Semigroup Complex where
-  (<>) c c'= let i = c.imaginary in Complex { real: 1, imaginary: i } 
- -- (<>) c c'= Complex (c.real + c'.real) (c.imaginary + c'.imaginary)
- -- (<>) c c'= Complex { real: 1, imaginary: (c.imaginary + c'.imaginary) } 
+   (<>) (Complex c) (Complex c') = Complex { real: c.real + c'.real, imaginary: c.imaginary + c'.imaginary }
+
+-- type synonym instances are not allowed. 
+--instance complexSemiGroup :: Semigroup Cplx where
+--   (<>) c c' = { real: c.real + c'.real, imaginary: c.imaginary + c'.imaginary }
