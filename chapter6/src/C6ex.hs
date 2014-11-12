8b888c5283f423a9dc5519541d37fb0f60ab796e
Tait Larson <telarson@gmail.com>
2014-11-12 09:24:15 -0500

    woring out NonEmpty issues in haskell.

--- chapter6/src/C6ex.hs
+++ chapter6/src/C6ex.hs
@@ -1,7 +1,18 @@
 module C6ex where
 
 import Data.Monoid
+import Data.Foldable
 
 data NonEmpty a = NonEmpty a [a]
 
+instance Foldable NonEmpty where
+--  foldr f b (NonEmpty x [])     = f x b
+--  foldr f b (NonEmpty x (a:as)) = f x (foldr f b (NonEmpty a as)) 
+
+  foldMap f (NonEmpty x (a:as)) = f x <> foldMap f (NonEmpty a as)
+  foldMap f (NonEmpty x [])     = f x
+--  foldMap f q m     = f x
+
+--	foldl f b (NonEmpty x [])     = f b x
+--  foldl f b (NonEmpty x (a:as)) = foldl f (f b x) $ NonEmpty a as 
 
